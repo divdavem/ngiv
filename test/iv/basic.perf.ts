ef48f317e1b35ad2e720ac0f4d128526003df525
Miško Hevery <misko@hevery.com>
2017-10-04 15:17:46 -0700

    test: add iteration to tests

--- test/iv/basic.perf.ts
+++ test/iv/basic.perf.ts
@@ -5,37 +5,39 @@ describe('iv perf test', () => {
 
   var window = createWindow('', 'http://localhost');
   var document = window.document;
-  const count = 10000;
+  const count = 100000;
   
   describe('render', () => {
-    it('should create ${count} divs in DOM', () => {
-      const start = new Date().getTime();
-      const container = document.createElement('div');
-      for(var i = 0; i < count; i++ ) {
-        const div = document.createElement('div');
-        div.appendChild(document.createTextNode('-'));
-        container.appendChild(div);
-      }
-      const end = new Date().getTime();
-      log('${count} DIVs in DOM', (end - start)/count);
-    });
-
-    it('should create ${count} divs in IV', () => {
-      const start = new Date().getTime();
-      const container = document.createElement('div');
-      render(createHostNode(container), document, Template, {});
-      const end = new Date().getTime();
-      log('${count} DIVs in IV', (end - start)/count);
-
-      function Template() {
+    for(var iteration = 0; iteration < 10; iteration++) {
+      it('${iteration}. create ${count} divs in DOM', () => {
+        const start = new Date().getTime();
+        const container = document.createElement('div');
         for(var i = 0; i < count; i++ ) {
-          ec('div');
-            tc('-');
-          ee();
+          const div = document.createElement('div');
+          div.appendChild(document.createTextNode('-'));
+          container.appendChild(div);
         }
+        const end = new Date().getTime();
+        log('${count} DIVs in DOM', (end - start)/count);
+      });
+  
+      it('${iteration}. create ${count} divs in IV', () => {
+        const start = new Date().getTime();
+        const container = document.createElement('div');
+        render(createHostNode(container), document, Template, {});
+        const end = new Date().getTime();
+        log('${count} DIVs in IV', (end - start)/count);
+  
+        function Template() {
+          for(var i = 0; i < count; i++ ) {
+            ec('div');
+              tc('-');
+            ee();
+          }
+        }
+  
+      });
       }
-
-    });
   });
 });
 
